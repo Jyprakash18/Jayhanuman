b80340997f7648c69efd68315c4068dd90abd157
Jyprakash18 <jayPrakash092006@gmail.com>
2026-05-05 10:53:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+API_ID = int(os.getenv("API_ID", 29270492))
+API_HASH = os.getenv("API_HASH", "c0ff4a728d5da8de25807766b2901111")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8674402252:AAGlHWN0pFWlX1vIwFhdXuUnE3akYiys4ik")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 7990200132))
 
 # Razorpay Config
 RZP_KEY_ID = os.getenv("RZP_KEY_ID", "")
